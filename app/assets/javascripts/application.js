6b74fd7ef82838c9921e170a76af98a835209878
Daniel Nascimento <dbnkocao@gmail.com>
2020-01-15 00:05:53 -0300

    try to fix error on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require tether/dist/js/tether
-//= require bootstrap/dist/js/bootstrap.bundle
+//= require bootstrap/dist/js/bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
